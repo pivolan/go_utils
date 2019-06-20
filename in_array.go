81b75c491eaadf87a6e1f2572131d30299d9444b
pivolan <pivolan@gmail.com>
2019-06-20 08:47:20 +0400

    isUrlParse

--- in_array.go
+++ in_array.go
@@ -1,5 +1,10 @@
 package go_utils
 
+import (
+	"fmt"
+	"net/url"
+)
+
 func InArray(val string, array []string) bool {
 	for _, row := range array {
 		if val == row {
@@ -8,4 +13,11 @@ func InArray(val string, array []string) bool {
 	}
 
 	return false
+}
+func IsUrlParse(str string) (*url.URL, error) {
+	u, err := url.Parse(str)
+	if err == nil && u.Scheme != "" && u.Host != "" && (u.Scheme == "http" || u.Scheme == "socks5") {
+		return url.Parse(str)
+	}
+	return nil, fmt.Errorf("not valid url proxy format")
 }
