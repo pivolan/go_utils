6ffbc32a2d0e9c865b6de19a8c550e205d41da62
pivolan <pivolan@gmail.com>
2019-06-27 14:07:09 +0400

    proxy filter, check proxy string

--- in_array.go
+++ in_array.go
@@ -21,3 +21,22 @@ func IsUrlParse(str string) (*url.URL, error) {
 	}
 	return nil, fmt.Errorf("not valid url proxy format")
 }
+func IsProxyUrl(str string) bool {
+	u, err := url.Parse(str)
+	return err == nil && u.Scheme != "" && u.Host != "" && u.Port() != "" && (u.Scheme == "socks5" || u.Scheme == "http")
+}
+func FilterValidProxies(proxies []string) (validProxies []string) {
+	for _, proxy := range proxies {
+		if IsProxyUrl(proxy) {
+			validProxies = append(validProxies, proxy)
+		}
+	}
+	return
+}
+func GetIpFromUrl(proxy string) string {
+	u, err := url.Parse(proxy)
+	if err != nil {
+		return ""
+	}
+	return u.Hostname()
+}
